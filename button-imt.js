e8cddcc17bdce83b4b0fa2635cb353e9116ec757
Utissa <tacafela@gmail.com>
2023-10-12 22:51:29 +0700

    Update logo icon bro

--- button-imt.js
+++ button-imt.js
@@ -2,14 +2,14 @@ const body = document.body;
 const buttonHeight = 50;
 const buttonWidth = 90;
 const maxHeight = window.innerHeight - buttonHeight;
-const rightOver = 900;
+const rightOver = 1000;
 const leftOver = 500;
 const nama = prompt("Siapa nama Kamu ?");
 
 window.addEventListener("DOMContentLoaded", () => {
   const noButton = document.getElementById("no-button");
   const yesButton = document.getElementById("yes-button");
-  yesButton.addEventListener("click", () => alert("love you too <3" +"  "+ nama));
+  yesButton.addEventListener("click", () => alert("love you too <3" + "  " + nama));
 
   noButton.addEventListener("mouseover", () => {
     noButton.style.left = Math.floor(Math.random() * (leftOver + 1)) + "px";
@@ -24,6 +24,5 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 const lyt = document.createElement("h3");
 lyt.innerHTML = "<b> Love You Too <3  </b>";
-function Lyt() {
-  body.append(lyt);
-}
+// function Lyt() {
+//   body.append(lyt);
